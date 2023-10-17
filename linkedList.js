afbcc33a81a31b587c11c553645fc4c28e7ddeed
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-10-16 20:17:02 -0500

    Add append function to linkedList

--- linkedList.js
+++ linkedList.js
@@ -1,13 +1,29 @@
 const LinkedList = (head = null) => {
+  const append = (value) => {
+    const newNode = Node(value);
+
+    if (!list.head) {
+      list.head = newNode;
+      return;
+    }
+
+    let current = list.head;
+    while (current.next !== null) {
+      current = current.next;
+    }
+    current.next = newNode;
+  };
+
   return {
     head,
+    append,
   };
 };
 
-const Node = (value) => {
+const Node = (value = null, next = null) => {
   return {
-    value: (value ? value : null),
-    next: null,
+    value,
+    next,
   };
 };
 
@@ -17,4 +33,7 @@ node1.next = node2;
 
 let list = LinkedList(node1);
 
+console.log(!list.head);
 console.log(list.head.next.value);
+list.append(12);
+console.log(list.head.next.next.value);
